8f965e18607c317ebabf9d33c78fdc7c470b21fd
Nebulafalls <1924383963@qq.com>
2024-11-08 19:19:38 +0800

    添加鼠标5s静止自动暂停检测，鼠标移动恢复检测

--- krita_obs_control.py
+++ krita_obs_control.py
@@ -1,9 +1,28 @@
 from PyQt5.QtWidgets import *
 from krita import *
 from .obswebsocket import *
+import threading
+import time
 
 webport=4455
 password="XoVO1rxSMY67xfbV"
+
+
+class MouseTracker(QObject):
+    mouseMoved = pyqtSignal(QMouseEvent)
+
+    def __init__(self):
+        super().__init__()
+        self.previous_position = None
+        
+
+    def eventFilter(self, obj, event):
+        if event.type() == QMouseEvent.MouseMove:
+            if self.previous_position != event.pos():
+                self.mouseMoved.emit(event)
+                self.previous_position = event.pos()
+        return super().eventFilter(obj, event)
+    
 
 class obs_control_Docker(DockWidget):
 
@@ -14,6 +33,7 @@ class obs_control_Docker(DockWidget):
         ws.call(requests.StartRecord())
         ws.disconnect()
         self.timer.start(1000)
+        self.start_tracking()
         
     def stop_record(self):
         ws=obsws("localhost", webport, password)
@@ -22,15 +42,33 @@ class obs_control_Docker(DockWidget):
         ws.disconnect()
         self.timer.stop()
         self.label.setText("录制已停止")
+        self.stop_tracking()
     
     def toggle_record(self):
         ws=obsws("localhost", webport, password)
         ws.connect()
         ws.call(requests.ToggleRecordPause())
         ws.disconnect() 
-    
+        
+    def pause_record(self):
+        ws=obsws("localhost", webport, password)
+        ws.connect()
+        ws.call(requests.PauseRecord())
+        ws.disconnect() 
+        
+    def resume_record(self):
+        ws=obsws("localhost", webport, password)
+        ws.connect()
+        ws.call(requests.ResumeRecord())
+        ws.disconnect() 
+        
     def __init__(self):
         super().__init__()
+        
+        self.tracker = MouseTracker()
+        self.tracker.mouseMoved.connect(self.on_mouse_moved)
+
+        
         self.setWindowTitle("obs控制")
         control_rec_Widget = QWidget(self)
         control_rec_Widget_v = QWidget(self)
@@ -61,6 +99,14 @@ class obs_control_Docker(DockWidget):
         self.timer.timeout.connect(self.update_label)
           # 每秒更新一次
         
+        self.timer2 = QTimer(self)
+        self.timer2.timeout.connect(self.track_mouse_move)
+        
+        #初始化鼠标移动检测
+        self.initial_position = None
+        
+        
+        
         
         self.setWidget(control_rec_Widget_v)
 
@@ -80,5 +126,27 @@ class obs_control_Docker(DockWidget):
             self.label.setText("录制已停止")
         #self.label.setText("obs状态: " + status ["outputTimecode"])
         ws.disconnect()
-
+        
+    def start_tracking(self):
+        self.timer2.start(5000)#启动鼠标静止检测
+        
+        app = QApplication.instance()
+        app.installEventFilter(self.tracker)
+        
+        
+    def stop_tracking(self):
+        self.timer2.stop()
+        
+    def get_mouse_position(self):
+        return Krita.instance().activeWindow().qwindow().cursor().pos()
+    
+    def track_mouse_move(self):
+        #self.label=self.get_mouse_position()
+        if self.initial_position == self.get_mouse_position():
+            self.pause_record()
+        self.initial_position = self.get_mouse_position()
+    
+    def on_mouse_moved(self, event):
+        self.resume_record()
+        
 Krita.instance().addDockWidgetFactory(DockWidgetFactory("obs_control_Docker", DockWidgetFactoryBase.DockRight, obs_control_Docker))
